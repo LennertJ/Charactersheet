715e65055f664ec5a0ab7ff115949f4372d88003
LennertJ <32568318+LennertJ@users.noreply.github.com>
2025-01-26 13:35:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function formatBonus(...score) {
 
 document.getElementById("barki").innerHTML = barki;
 //blade song
-document.getElementById("acModifier").innerHTML = formatBonus(intModifier+1);
+document.getElementById("acModifier").innerHTML = formatBonus(intModifier,1);
 //core stats
 document.getElementById("baseAc").innerHTML = 13 + dexModifier;
 document.getElementById("hp").innerHTML = hp;
